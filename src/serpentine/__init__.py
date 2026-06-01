a29f79ccb1d7b04f71c3e2b5e5136de1289134f3
Chris Garretty <cgarretty@gmail.com>
2026-06-01 01:30:35 -0400

    bump python app to v2.1.0

--- src/serpentine/__init__.py
+++ src/serpentine/__init__.py
@@ -22,7 +22,7 @@ Or programmatically:
     app = create_app(state)
 """
 
-__version__ = "0.1.6"
+__version__ = "0.2.1"
 
 from serpentine.server import create_app
 from serpentine.state import GraphStateManager
